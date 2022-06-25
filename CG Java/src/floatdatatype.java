a1287673a2939b3100f77a9b0ce7e2970e811cb2
ganes <ganes@DESKTOP-OPBLK4F.bbrouter>
2022-06-25 17:02:41 +0530

    Data types

---
+++ CG Java/src/floatdatatype.java
@@ -0,0 +1,19 @@
+
+public class floatdatatype {
+
+	public static void main(String[] args) {
+		int value1 = 8/2;
+		float value2 = 12f/6f;
+		double value3 = 10d/5d;
+		System.out.println("value  1 = "+ value1);
+		System.out.println("value  2 = "+ value2);
+		System.out.println("value  3 = "+ value3);
+	    int marker = 250;
+	    double percentage = marker * 0.46f;
+	    
+	    System.out.println("percentage:"+percentage);
+		
+
+	}
+
+}
