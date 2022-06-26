50129ea29d2b666a57c4199be1b589165256b96d
ganes <ganes@DESKTOP-OPBLK4F>
2022-06-26 16:00:44 +0530

    operators

---
+++ CG Java/src/assignmentoperatorexample.java
@@ -0,0 +1,17 @@
+
+public class assignmentoperatorexample {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 10;
+		int z = 15;
+		
+		x%=5;
+		y/=10;
+		z*=15;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+
+}
