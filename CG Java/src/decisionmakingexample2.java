d00dedd2207dfdfa28d1cc90b5a536dd3392cf60
ganes <ganes@DESKTOP-OPBLK4F>
2022-06-26 15:44:08 +0530

    decisionmakingexample2

---
+++ CG Java/src/decisionmakingexample2.java
@@ -0,0 +1,18 @@
+
+public class decisionmakingexample2 {
+
+	public static void main(String[] args) {
+		int ageOfBoy = 36;
+		int ageOfGirl = 25;
+		
+		if((ageOfBoy>=21)&&(ageOfGirl>=18))
+		{
+			System.out.println("Ready to Get Married!");
+		}
+		else
+		{
+			System.out.println("Wait foa a Kiddo");
+		}
+	}
+
+}
