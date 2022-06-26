c7a49106b8afae8bbf014ae97f1fc30c28bdf422
ganes <ganes@DESKTOP-OPBLK4F>
2022-06-26 15:05:35 +0530

    decisionmaking 1 example

---
+++ CG Java/src/decisionmakingexample.java
@@ -0,0 +1,35 @@
+
+public class decisionmakingexample {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 8;
+		int z = 7;
+	if(x!=5)
+	{ 
+		System.out.println("Value of x is not 5");
+	}
+	else
+	{
+		System.out.println("Value of x is 5");
+	}
+	if(x>=z) 
+	{
+		System.out.println("True");
+	}
+	else 
+	{
+		System.out.println("False");
+	}
+	if(!(x<y)||(y==z))
+	{
+		System.out.println("Condition is TRUE");
+	}
+	else
+	{
+		System.out.println("Condition is False");
+	}
+		
+	}
+
+}
