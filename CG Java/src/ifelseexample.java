f3776d0c114b7ba1285f377ebe0ef0ad620c2b5b
ganes <ganes@DESKTOP-OPBLK4F>
2022-06-26 16:15:23 +0530

    if else example

---
+++ CG Java/src/ifelseexample.java
@@ -0,0 +1,32 @@
+
+public class ifelseexample {
+
+	public static void main(String[] args) {
+		int x = 10;
+		
+		if(x==10)
+		{
+		if(x>=10)
+		{
+			System.out.println("X is greater than 10");
+		}
+		else
+		{
+			System.out.println("X is Smaller than 10");
+		}
+		} 
+		if(x==10)
+		{
+			System.out.println("X is equals to 10");
+		}
+		else if(x>10)
+		{
+			System.out.println("X is greater than  10");
+		}
+		else
+		{
+			System.out.println("X is smaller than 10");
+		}
+	}
+
+}
