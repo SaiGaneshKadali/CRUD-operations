44c5d3d3005f78c187874011765f42336fe416bc
ganes <ganes@DESKTOP-OPBLK4F.bbrouter>
2022-06-27 12:38:31 +0530

    typecasting

--- CG Java/src/typecastingexample.java
+++ CG Java/src/typecastingexample.java
@@ -4,10 +4,10 @@ public class typecastingexample {
 	public static void main(String[] args) {
 		int i = 10;
 		long l = (long)i;
-		double j = 15.5;
-		float f = (float)j;
+		double d2 = 1.234e2;
+		float f2 = (float)d2;
 	System.out.println(l);
-	System.out.println(f);
+	System.out.println(f2);
 	}
 
 }
