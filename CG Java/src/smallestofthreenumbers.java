5a92fea631a779e695c8f13188ec9a70afc6212a
ganes <ganes@192.168.0.103>
2022-07-15 11:36:15 +0530

    smallest of three numbers

---
+++ CG Java/src/smallestofthreenumbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class smallestofthreenumbers {
+		private static Scanner sc;
+		public static void main(String[] args) {
+			int x, y, z;
+			sc = new Scanner(System.in);		
+			System.out.println("Please Enter three Numbers: ");
+			x = sc.nextInt();
+			y = sc.nextInt();
+			z = sc.nextInt();
+			
+			if (x < y && x < z) {
+				System.out.format("\n%d is the Smallest Number", x);
+			}
+			else if (y < x && y < z) {
+				System.out.format("\n%d is the Smallest Number", y);
+			}	
+			else if (z < x && z < y) {
+				System.out.format("\n%d is the Smallest Number", z);
+			}		
+			else {
+				System.out.println("\nEither any 2 values or all of them are equal");
+			}
+		}
+	}
